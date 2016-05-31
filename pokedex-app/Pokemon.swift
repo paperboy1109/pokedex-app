e216e8de856aa147738e6ec3014be8d3876ee5c0
Daniel J Janiak <danieljjaniak@Daniels-MacBook-Air.local>
2016-05-31 17:37:38 -0600

    Create data model

---
+++ pokedex-app/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex-app
+//
+//  Created by Daniel J Janiak on 5/31/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
