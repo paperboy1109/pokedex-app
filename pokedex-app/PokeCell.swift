3536dd256906683bc3a8b605115d7cbf1283dc79
Daniel J Janiak <danieljjaniak@Daniels-MacBook-Air.local>
2016-05-31 18:31:44 -0600

    Create custom cell

---
+++ pokedex-app/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  pokedex-app
+//
+//  Created by Daniel J Janiak on 5/31/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+
+}
