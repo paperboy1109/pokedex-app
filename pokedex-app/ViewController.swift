5f87a0976d0b00d2f60c05f68b2ece2052664fc7
Daniel J Janiak <danieljjaniak@Daniels-MacBook-Air.local>
2016-06-01 14:27:46 -0600

    Parse data from CSV file

--- pokedex-app/ViewController.swift
+++ pokedex-app/ViewController.swift
@@ -11,21 +11,49 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collection: UICollectionView!
-
+    
+    var pokemon = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collection.delegate = self
         collection.dataSource = self
         
+        parsePokemonCSV()
+        
+    }
+    
+    func parsePokemonCSV() {
+        let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
+        
+        do {
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            print(rows)
+            
+            for row in rows {
+                // let pokeID = Int(row["id"]!)!
+                if let pokeID = Int(row["id"]!) {
+                    let name = row["identifier"]!
+                    let poke = Pokemon(name: name, pokedexId: pokeID)
+                    
+                    pokemon.append(poke)
+                }
+            }
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
             
-            let pokemon = Pokemon(name: "test", pokedexId: indexPath.row)
+            // let pokemon = Pokemon(name: "test", pokedexId: indexPath.row)
+            let poke = pokemon[indexPath.row]
             
-            cell.configureCell(pokemon)
+            // cell.configureCell(pokemon)
+            cell.configureCell(poke)
             
             return cell
             
